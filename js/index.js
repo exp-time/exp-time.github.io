819e95a9b8b9d3fa987cece75f7e6381086aa21c
exp-time <166214763+exp-time@users.noreply.github.com>
2024-04-08 07:40:34 +0300

    test

--- js/index.js
+++ js/index.js
@@ -6,8 +6,8 @@ function w3_open(id) {
   x.style.fontSize = "20px";
   x.style.paddingTop = "0%";
   x.style.display = "block";
+  x.style.backgroundColor = x.style.backgroundColor = "var(.w3-theme-l4)";
 }
-
 
 function info_open(id) {
   var x = document.getElementById(id);
