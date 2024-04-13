d53444a2966cb95b1626fe53a36152eb5b441326
exp-time <166214763+exp-time@users.noreply.github.com>
2024-04-13 09:42:31 +0300

    test

--- js/index.js
+++ js/index.js
@@ -99,3 +99,32 @@ function includeHTML() {
     }
   }
 }
+
+// Create main content cards
+function createCard(id, title, iconClass, content) {
+  // Card
+  var card = document.createElement('div');
+  card.className = 'w3-card w3-container';
+
+  // Row for title
+  var row = document.createElement('div');
+  var dummyButton = document.createElement('div');
+  var titleDiv = document.createElement('div');
+  var infoIcon = document.createElement('a');
+
+  row.className = 'w3-row w3-row-padding w3-xlarge container-title flex-container';
+  dummyButton.className = 'dummy-button';
+  titleDiv.className = "title";
+  titleDiv.textContent = 'API Endpoints';
+  infoIcon.onclick = function() { info_open('first_info'); }; 
+  infoIcon.className = 'fa fa-info w3-button top-corner';
+
+  var paragraph = document.createElement('p');
+  var selectedIcon = document.createElement('i');
+  paragraph.textContent = content;
+  selectedIcon.className = iconClass + ' w3-margin-bottom w3-text-theme';
+
+  row.append(dummyButton, titleDiv, infoIcon);
+  card.append(row, selectedIcon, paragraph);
+  document.getElementById(id).appendChild(card);
+}
---
+++ js/mainCards.js
@@ -0,0 +1,74 @@
+const cardData = [
+    {
+        id: "content-area1",
+        title: "API Endpoints",
+        iconClass: "fa fa-terminal",
+        content: "Custom built, with effortless use. Data-products for seamless integration. Advanced applications and tangible value generation."
+    },
+    {
+        id: "content-area2",
+        title: "Networks",
+        iconClass: "fa fa-code-fork",
+        content: "Network analysis"
+    },
+    {
+        id: "content-area3",
+        title: "Artificial Intelligence",
+        iconClass: "fa fa-cog",
+        content: "Advanced AI methods"
+    },
+    {
+        id: "content-area4",
+        title: "Deep Tech",
+        iconClass: "fa fa-spinner",
+        content: "Solutions from the cutting edge. Quantum Computing."
+    },
+    {
+        id: "content-area5",
+        title: "Advanced Mathematics",
+        iconClass: "fa fa-superscript",
+        content: "Cutting edge optimization."
+    },
+    {
+        id: "content-area6",
+        title: "Analytics",
+        iconClass: "fa fa-bar-chart",
+        content: "Combine API's with analytics."
+    },
+    {
+        id: "content-area7",
+        title: "TODO TEST",
+        iconClass: "fa fa-check-square",
+        content: "TODO TEST"
+    },
+    {
+        id: "content-area8",
+        title: "TODO TEST",
+        iconClass: "fa fa-check-square",
+        content: "TODO TEST"
+    },
+    {
+        id: "content-area9",
+        title: "TODO TEST",
+        iconClass: "fa fa-check-square",
+        content: "TODO TEST"
+    },
+    {
+        id: "content-area10",
+        title: "TODO TEST",
+        iconClass: "fa fa-check-square",
+        content: "TODO TEST"
+    },
+    {
+        id: "content-area11",
+        title: "TODO TEST",
+        iconClass: "fa fa-check-square",
+        content: "TODO TEST"
+    },
+    {
+        id: "content-area12",
+        title: "TODO TEST",
+        iconClass: "fa fa-check-square",
+        content: "TODO TEST"
+    },
+];
