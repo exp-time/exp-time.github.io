b2ebc44eb9e9fef660880f38b8b6ea99829fe0c9
exp-time <166214763+exp-time@users.noreply.github.com>
2024-04-13 08:48:10 +0300

    test

--- js/elemGen.js
+++ js/elemGen.js
@@ -2,19 +2,17 @@ function createCard(title, content, iconClass) {
     // Card
     var card = document.createElement('div');
     card.className = 'w3-card w3-container';
+
     // Row for title
     var row = document.createElement('div');
-    row.className = 'w3-row w3-row-padding w3-xlarge container-title flex-container';
-    // Dummy button (placeholder)
     var dummyButton = document.createElement('div');
-    dummyButton.className = 'dummy-button';
-    // Title
     var titleDiv = document.createElement('div');
+    var infoIcon = document.createElement('a');
+
+    row.className = 'w3-row w3-row-padding w3-xlarge container-title flex-container';
+    dummyButton.className = 'dummy-button';
     titleDiv.className = title;
     titleDiv.textContent = 'API Endpoints';
-
-    // Info icon
-    var infoIcon = document.createElement('a');
     infoIcon.onclick = function() { info_open('first_info'); }; 
     infoIcon.className = 'fa fa-info w3-button top-corner';
 
