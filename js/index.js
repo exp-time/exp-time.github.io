6d094e2bb4a51f1612d168d18489d88a92750640
exp-time <166214763+exp-time@users.noreply.github.com>
2024-04-15 13:45:09 +0300

    test

--- js/index.js
+++ js/index.js
@@ -17,7 +17,7 @@ function closeModal(event, id) { // Close if click outside of modal
 function headerWithClose(id, title) { // Create header and close button
   const header = createElementWithClass('header', 'modal-header')
   const headerP = createElementWithClass('p', '', title);
-  const closeButton = createElementWithClass('button', 'w3-bar-item w3-button');
+  const closeButton = createElementWithClass('button', 'w3-bar-item w3-button w3-display-topright');
   const closeIcon = createElementWithClass('i', 'fa fa-remove');
   closeButton.appendChild(closeIcon);
   closeButton.onclick = () =>  info_close(id);
