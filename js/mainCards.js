85218d2e3230752e35a4c0f60f5c73fec6a25644
exp-time <166214763+exp-time@users.noreply.github.com>
2024-04-15 11:16:53 +0300

    test

--- js/mainCards.js
+++ js/mainCards.js
@@ -96,7 +96,7 @@ const modalData = [
         id: "about_modal",
         title: "About section",
         content: "In computational complexity theory, the complexity class EXPTIME (sometimes called EXP or DEXPTIME) is the set of all decision problems that are solvable by a deterministic Turing machine in  exponential time, i.e., in O(2ᵖ⁽ⁿ⁾) time, where p(n) is a polynomial function of n[1].",
-        foooterContent: "<a class='w3-button padding-none'onclick='openInNewTab('https://en.wikipedia.org/wiki/EXPTIME')'> [1].</a>", 
+        foooterContent: "AAAAAAAAA",//"<a class='w3-button padding-none'onclick='openInNewTab('https://en.wikipedia.org/wiki/EXPTIME')'> [1].</a>", 
     },
     {
         id: "unfinished_modal",
