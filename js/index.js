6f9f68e8473b9544820591232a9c9975f41fc234
exp-time <166214763+exp-time@users.noreply.github.com>
2024-04-18 12:06:20 +0300

    test

--- js/index.js
+++ js/index.js
@@ -128,7 +128,13 @@ function createCard(id, info_id, title, iconClass, content, info_Title, info_Con
       ["Jane Doe", "34", "Designer", "San Francisco"],
       ["Jane Doe", "34", "Designer", "San Francisco"],
       ["Jane Doe", "34", "Designer", "San Francisco"],
+      ["Jane Doe", "34", "Designer", "San Francisco"],
+      ["Jane Doe", "34", "Designer", "San Francisco"],
+      ["Jane Doe", "34", "Designer", "San Francisco"],
+      ["Jane Doe", "34", "Designer", "San Francisco"],
+      ["Jane Doe", "34", "Designer", "San Francisco"],
       ["Jane Doe", "34", "Designer", "San Francisco"]
+      
   ]);
   }
   else {
