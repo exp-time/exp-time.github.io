1ef6f269d6f692a5b9638133200db17dec3c71bc
exp-time <166214763+exp-time@users.noreply.github.com>
2024-04-21 08:39:59 +0300

    test

--- js/index.js
+++ js/index.js
@@ -50,7 +50,7 @@ function createTooltipIcon(link, title, icon) {
 
 function createSidebar(id, title, content) { // Sidebar popups
   const sidebar = createElementWithClass('nav', 'w3-sidebar w3-card w3-animate-left w3-center');
-  const sidebarContent = createElementWithClass('div', 'sidebar-content')
+  const sidebarContent = createElementWithClass('div', 'sidebar-content w3-bar-block')
   const header = headerWithClose(id, title, "w3-theme-l1")
   sidebar.appendChild(header);
   for (const key in content) {
