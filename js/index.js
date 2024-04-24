87472b399d0da973ef56f2f8a4a711bc6da9ccbc
exp-time <166214763+exp-time@users.noreply.github.com>
2024-04-24 10:19:53 +0300

    test

--- js/index.js
+++ js/index.js
@@ -152,12 +152,12 @@ function createCard(id, info_id, title, iconClass, content, info_Title, info_Con
 }
 
 document.addEventListener('DOMContentLoaded', function() { // pagination + others
-  /* REMOVE */
-  document.getElementById('unfinished_modal').style.display='block'
-  /* REMOVE */
   modalData.forEach(function(modal) {
     createModal(modal.id, modal.title, modal.content, modal.footerContent);
   });
+    /* REMOVE */
+  document.getElementById('unfinished_modal').style.display='block'
+    /* REMOVE */
   sideBarData.forEach(function(item) {
     createSidebar(item.id, item.title, item.content);
   }); 
