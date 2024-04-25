3429a35924f3969c4c90335e4bdee52c32317086
exp-time <166214763+exp-time@users.noreply.github.com>
2024-04-25 08:54:13 +0300

    test

--- js/styles.js
+++ js/styles.js
@@ -1,7 +1,3 @@
-function s_Font_Small(element) {
-  element.style["font-size"] = '0.5vw !important';
-}
-
 function s_Sidebar(element) {
   element.style["width"] = '35%';
   element.style["top"] = '5%';
