04db9f36a457cbecee4f40830db971e18a92c6f1
exp-time <166214763+exp-time@users.noreply.github.com>
2024-04-27 08:25:22 +0300

    test

--- js/index.js
+++ js/index.js
@@ -148,10 +148,12 @@ function createCard(id, info_id, title, iconClass, content, info_Title, info_Con
   ]);
   }
   else if (content === 2){
-
+    // TODO HANDLE
+    paragraph = ""
   }
   else {
     // TODO HANDLE
+    paragraph = ""
   }
   if (iconClass != '') {
     const selectedIcon = createElementWithClass('i', iconClass + ' w3-margin font-mega w3-text-theme');
