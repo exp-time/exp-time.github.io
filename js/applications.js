595d6ca6b8364120a588a56d815d16f6378cb204
exp-time <166214763+exp-time@users.noreply.github.com>
2024-05-16 12:27:18 +0300

    test

--- js/applications.js
+++ js/applications.js
@@ -284,10 +284,10 @@ function updateIconsOnMap(id, map) {
   for (let i = 0; i < numberOfIcons; i++) {
     const position = i  * (lineLength / numberOfIcons);
     const point = L.GeometryUtil.interpolateOnLine(map, polylines[id].getLatLngs(), position / lineLength);
-    console.log(point)
+    //console.log(point)
     if (!point) continue; // Skip if no point is returned
     // Fetch weather data for each interpolated point
-    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${point.latLng.lat},${point.latLng.lng}?key=${apiKey}`
+    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${point.latLng.lat},${point.latLng.lng}?key=${api_key_w}`
     fetch(url).then(response => response.json()).then(data => {
       const iconUrl = `img/weatherIcons/${data.currentConditions.icon}.png`;
       const icon = L.icon({
